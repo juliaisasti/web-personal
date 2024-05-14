fada8b3b1e45631d1dfa708da3ee64810982202d
juliaisasti <juliaisasti@gmail.com>
2024-05-14 18:32:20 +0200

    home fonts added and basic mixin for headers

--- src/assets/components/organisms/MainHomeComponent.jsx
+++ src/assets/components/organisms/MainHomeComponent.jsx
@@ -11,35 +11,43 @@ const MainHomeComponent = () => {
 
   return (
     <>
-      <div className="MenuComponent">
-        <button onClick={handleClick}>MENU</button>
+      <div className="HomeContent">
+        <button onClick={handleClick} className="MenuButton">
+          MENU
+        </button>
 
-        {toggle ? (
-          <nav className="MenuComponentList">
-            <h3>Where do you wanna go?</h3>
-            <h1>
-              <Link to="/">Home</Link>
-            </h1>
-            <h1>
-              <Link to="/about">About</Link>
-            </h1>
-            <h1>
-              <Link to="/projects">Projects</Link>
-            </h1>
-            <h1>
-              <Link to="/contact">Contact</Link>
-            </h1>
-            <button>Give me love</button>
-          </nav>
-        ) : (
-          <></>
-        )}
+        <div className="HomeDescription">
+          <h1>
+            <span className="home-title">Hey!</span> I'm Julia Isasti.
+          </h1>
+          <h1>
+            Look, just because I <span className="home-title">developer</span>{" "}
+            no man a foot <span className="home-title">full-stack</span> don't
+            make it right for Marsellus.
+          </h1>
+        </div>
       </div>
-      <h1>Hey! I'm Julia Isasti.</h1>
-      <h1>
-        Look, just because I developer no man a foot full-stack don't make it
-        right for Marsellus.
-      </h1>
+
+      {toggle ? (
+        <nav className="MenuComponentList">
+          <h3>Where do you wanna go?</h3>
+          <h1>
+            <Link to="/">Home</Link>
+          </h1>
+          <h1>
+            <Link to="/about">About</Link>
+          </h1>
+          <h1>
+            <Link to="/projects">Projects</Link>
+          </h1>
+          <h1>
+            <Link to="/contact">Contact</Link>
+          </h1>
+          <button className="LoveButton">Give me love</button>
+        </nav>
+      ) : (
+        <></>
+      )}
     </>
   );
 };
